7c39a01170aea3453613125c58aee0c86e9ba8b5
Isadora do Costa Romero Maceió <isadora.maceio@dte.local>
2019-11-01 16:16:27 -0300

    refatora método filter em getConcluido

--- src/controllers/tarefasController.js
+++ src/controllers/tarefasController.js
@@ -1,55 +1,62 @@
 const tarefas = require("../model/listaTarefas.json")
 
-exports.get = (req,res) =>{
+exports.get = (req, res) => {
     console.log(req.url)
 
     res.status(200).send(tarefas)
-  }
+}
 
 exports.getById = (req, res) => {
     const id = req.params.id
     const task = tarefas.find(task => task.id == id)
-    
+
     res.status(200).send(task)
- }
-
-exports.getConcluido = (req, res) =>{
-
-    const conc = tarefas.filter((item) =>{
-        return item.concluido === "true";
-    })
-    res.status(200).send(conc)
 }
+
+exports.getConcluido = (req, res) => {
+    
+    const conc = tarefas.filter(item => item.concluido)
+        return conc;
+    // const conc = tarefas.filter((item) => {
+    //     return item.concluido === "true";
+    res.status(200).send(conc)
+
+}   
 exports.getByNome = (req, res) => {
     const nomeColaborador = req.params.nome
-    const nomeBusca = tarefas.filter(nomeBusca  => nomeBusca.nomeColaborador === nomeColaborador)
-    
-    res.status(200).send(nomeBusca)
- }
-   
- exports.getByTempo = (req, res) =>{
+    const nomeBusca = tarefas.filter(nomeBusca => nomeBusca.nomeColaborador === nomeColaborador)
 
-    const inclusao1 = tarefas.dataInclusao.split('/')
-    const day1 = inclusao[0];
-    const month1 = inclusao[1];
-    const year1 = inclusao[2];
-    const dataNova1 = new Date(year1, month1 - 1, day1);
+    res.status(200).send(nomeBusca)
+}
+
+exports.getByTempo = (req, res) => {
+
+    /*const inclusaoA = tarefas.dataInclusao
+
+    const dataNova1 = new Date(inclusaoA);
+    const dataNova2 = new Date(inclusaoA[0], inclusaoA[1] - 1, inclusaoA[2]);
 
     console.log(dataNova1)
-    
-    const inclusao2 = tarefas.dataInclusao.split('/')
-    const day2 = inclusao2[0];
-    const month2 = inclusao2[1];
-    const year2 = inclusao2[2];
-    const dataNova2 = new Date(year2, month2 - 1, day2);
-
     console.log(dataNova2)
-    
-    function ordemCresc (a,b) {
-        return a.dataNova1 > b.dataNova2;
-    }
-    const ordenar = listarTarefas.sort(ordemCresc)
+
+    const ordenar = dataInclusao.map(function(a, b) {
+        const dataSplitA = a.dataInclusao.split('/');
+        const dataSplitB = b.dataInclusao.split('/');
+        const dataA = new Date(dataSplitA[2], dataSplitA[1], dataSplitA[0]);
+        const dataB = new Date(dataSplitB[2], dataSplitB[1], dataSplitB[0]);
+
+        return dataA - dataB.sort()
+
+         res.status(200).send(ordenar)
+    }*/
+
+    const ordenar = tarefas.sort((a,b) => {
+        var dataQuebradaA = a.dataInclusao.split('/');
+        var dataQuebradaB = b.dataInclusao.split('/');
+        var dataA = new Date(dataQuebradaA[2], dataQuebradaA[1], dataQuebradaA[0]);
+        var dataB = new Date(dataQuebradaB[2], dataQuebradaB[1], dataQuebradaB[0]);
+        return dataA - dataB
+    });
 
     res.status(200).send(ordenar)
-   
- }
+}
